dbc29e79eb20d984e4a9727233ac908cafa29d07
LibertasSupraOmnia <keatonh99@gmail.com>
2020-10-31 16:18:13 -0700

    fixing parts of hex.ah and testhex.a w/ _runtests.sh update

--- tests/_runtests.sh
+++ tests/_runtests.sh
@@ -11,3 +11,9 @@ nasm testgenreg.a -o testgenreg.o -felf64
 tcc testgenreg.o -o testgenreg
 ./testgenreg
 rm testgenreg testgenreg.o
+
+#test hex.ah
+nasm testhex.a -o testhex.o -felf64
+tcc testhex.o -o testhex
+./testhex
+rm testhex testhex.o
